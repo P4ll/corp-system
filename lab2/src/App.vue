77d48011185e1167eb8c3e5c7c00ec6f0b528ad6
P4L <cornelius.weling@gmail.com>
2020-05-01 13:27:20 +0400

    move menu to components

--- lab2/src/App.vue
+++ lab2/src/App.vue
@@ -1,30 +1,6 @@
 <template>
     <q-layout view="hHh lpR fFf">
-        <q-header bordered class="bg-primary text-white">
-            <q-toolbar>
-                <q-btn dense flat round icon="menu" @click="left = !left" />
-
-                <q-toolbar-title>
-                    Лабораторная работа
-                </q-toolbar-title>
-            </q-toolbar>
-        </q-header>
-
-        <q-drawer
-            v-model="left"
-            side="left"
-            overlay
-            behavior="desktop"
-            bordered
-        >
-            <!-- drawer content -->
-            <q-list>
-                <q-item>
-                    123
-                </q-item>
-            </q-list>
-        </q-drawer>
-
+        <my-menu/>
         <q-page-container>
             <router-view />
         </q-page-container>
@@ -32,14 +8,19 @@
 </template>
 
 <script>
+import MyMenu from './components/Menu.vue'
+
 export default {
+    components: {
+        MyMenu
+    },
+
     mounted() {
       document.title = "Лабораторная работа";
     },
 
     data() {
         return {
-            left: false,
         };
     },
 };
---
+++ lab2/src/components/Menu.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <q-header bordered class="bg-primary text-white">
+            <q-toolbar>
+                <q-btn dense flat round icon="menu" @click="left = !left" />
+
+                <q-toolbar-title>
+                    Лабораторная работа
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-header>
+
+        <q-drawer
+            v-model="left"
+            side="left"
+            overlay
+            behavior="desktop"
+            bordered
+        >
+            <!-- drawer content -->
+            <q-list>
+                <q-item>
+                    123
+                </q-item>
+            </q-list>
+        </q-drawer>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            left: false,
+        }
+    }
+}
+</script>
